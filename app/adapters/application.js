97687d164ad12397eef3dcf7df1a86d0229376ae
Kyle <kyle.simon.tong@gmail.com>
2019-04-25 20:31:33 +1200

    Remove firebase and use a rest adapter with my custom backend instead

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,12 @@
-import FirestoreAdapter from 'emberfire/adapters/firestore';
+import DS from 'ember-data';
+import { computed } from '@ember/object';
 
-export default FirestoreAdapter.extend({
+export default DS.RESTAdapter.extend({
+    host: 'http://localhost:8000',
+    namespace: 'api',
+    headers: computed(function () {
+        return {
+            'x-authentication': 'e13a991963cca255ec5b297e28e8d051'
+        }
+    })
 });
