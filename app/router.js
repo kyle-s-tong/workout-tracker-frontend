dbf8cea3c1170c86011c850447526e55fafafa3b
Kyle <kyle.simon.tong@gmail.com>
2019-06-18 18:25:22 +1200

    Get routine set up and start adding tiles to it

--- app/router.js
+++ app/router.js
@@ -10,6 +10,7 @@ Router.map(function() {
   this.route('workouts');
   this.route('login');
   this.route('register');
+  this.route('routines');
 });
 
 export default Router;
--- mirage/factories/routine.js
+++ mirage/factories/routine.js
@@ -1,8 +1,8 @@
 import { Factory } from 'ember-cli-mirage';
-import faker from 'faker';
+
 
 export default Factory.extend({
   title(i) {
-    return `${faker.lorem.word()} ${i}`
+    return `Routine ${i}`
   }
 });
