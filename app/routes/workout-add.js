5c645fa26ae792c0063f351e1ecdf619855f8545
Kyle <kyle.simon.tong@gmail.com>
2019-07-20 11:36:10 +1200

    Add model hook for adding workout to create record

--- app/routes/workout-add.js
+++ app/routes/workout-add.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
+  model() {
+    return this.store.createRecord('workout');
+  }
 });
