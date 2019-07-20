cd14d9f672f382dae6d52cc0f6fb2a4789f3c7fc
Kyle <kyle.simon.tong@gmail.com>
2019-07-21 11:01:47 +1200

    Add button for starting a workout. Rename the exercise route to exercise summary because it just gives information about the exercise, exercises need to be done in the workout session

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -8,7 +8,7 @@ export default function(server) {
   server.createList('user', 2).forEach(user => {
     server.createList('routine', 2, { user }).forEach(routine => {
       server.createList('workout', 2, { routines: [routine] }).forEach(workout => {
-        server.createList('exercise', 2, { workouts: [workout] })
+        server.createList('exercise', 5, { workouts: [workout] })
       })
     })
   });
