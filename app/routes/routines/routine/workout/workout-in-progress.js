f3e498baf612cce29e5034ffdd858abc0c080fae
Kyle <kyle.simon.tong@gmail.com>
2019-07-28 14:15:33 +1200

    Get the swiping working, now need to figure out how to create exercise records.

--- app/routes/routines/routine/workout/workout-in-progress.js
+++ app/routes/routines/routine/workout/workout-in-progress.js
@@ -1,15 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model(params) {
-    const record = this.store.createRecord('workout-record',
-      {
-        workout: this.store.findRecord('workout', params.current_workout_id)
-      }
-    )
-    record.save()
-      .then(record => {
-        return record;
-      })
-  }
 });
