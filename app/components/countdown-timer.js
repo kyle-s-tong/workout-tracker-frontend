0d4527488dd9b1fd573e0d6aad2ceb5f75e13d1f
Kyle <kyle.simon.tong@gmail.com>
2019-08-06 07:48:44 +1200

    Parse times for the rest timer to format it nicely

--- app/components/countdown-timer.js
+++ app/components/countdown-timer.js
@@ -3,6 +3,24 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   formattedRestTime: computed('restTime', function() {
-    return `00:${this.restTime}`
-  })
+    const time = new Date(null);
+    time.setSeconds(parseInt(this.restTime));
+
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    if (minutes && seconds) {
+      return `${this.padTimeString(minutes, '0', 2)}:${this.padTimeString(seconds, '0', 2)}`;
+    } else if (minutes && !seconds) {
+      return `${this.padTimeString(minutes, '0', 2)}:00`;
+    } else if (!minutes && seconds) {
+      return `00:${this.padTimeString(seconds, '0', 2)}`;
+    }
+
+    return null;
+  }),
+
+  padTimeString(string, pad, length) {
+    return (new Array(length + 1).join(pad) + string).slice(-length);
+}
 });
