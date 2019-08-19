5b55c648067300bd44184aec6843eeda42323574
Kyle <kyle.simon.tong@gmail.com>
2019-08-20 07:14:56 +1200

    Added templates to the routine edit and delete routes

--- app/controllers/routines/routine/routine-delete.js
+++ app/controllers/routines/routine/routine-delete.js
@@ -1,4 +1,14 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+    delete: async function() {
+      const routine = await this.model.workout;
+
+      this.model.destroyRecord()
+        .then(() => {
+          this.transitionToRoute('routines.routine', routine.id)
+     })
+    }
+  }
 });
