1f753b940117b8983079bf8ee1e9f89737636bc7
Kyle <kyle.simon.tong@gmail.com>
2019-08-27 07:09:47 +1200

    Add some changes to routines to match the backend schema

--- app/models/routine.js
+++ app/models/routine.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 const { Model, attr, hasMany, belongsTo } = DS;
 
 export default Model.extend({
-  title: attr('string'),
+  name: attr('string'),
   isActive: attr('boolean'),
   workouts: hasMany('workout'),
   user: belongsTo('user')
