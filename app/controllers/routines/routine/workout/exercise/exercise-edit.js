e247381137d230b0592f7caab2d461a35b331ae9
Kyle <kyle.simon.tong@gmail.com>
2019-09-05 20:05:49 +1200

    Get editing of supersets working for editing exercises. Still need to add it in for adding them.

--- app/controllers/routines/routine/workout/exercise/exercise-edit.js
+++ app/controllers/routines/routine/workout/exercise/exercise-edit.js
@@ -1,10 +1,7 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
 
 
 export default Controller.extend({
-  supersetExercises: null,
-
   actions: {
     save: function(model) {
       model.save()
@@ -15,20 +12,6 @@ export default Controller.extend({
       })
     },
     enteredRoute: function() {
-      this.formatSupersetExercises();
     }
-  },
-
-  formatSupersetExercises: async function() {
-    const superset = await this.model.superset;
-    let supersetsForForm = [];
-
-    if (superset) {
-      superset.forEach(exercise => {
-        supersetsForForm.push(exercise);
-      })
-    }
-
-    this.set('supersetExercises', supersetsForForm);
   }
 });
